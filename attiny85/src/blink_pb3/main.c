61a8b4dcda4e2228d8a583935051dd88fd7bf3b1
yvv <yvv4git>
2020-08-30 15:31:36 +0300

    Modify code

--- attiny85/src/blink_pb3/main.c
+++ attiny85/src/blink_pb3/main.c
@@ -9,26 +9,69 @@
  
 int main (void)
 {
-  // set PB3 to be output
     DDRB = 0b00001000;
+
+    // main loop
     while (1) {
     
-        // flash# 1:
-        // set PB3 high
-        PORTB = 0b00001000; 
-        _delay_ms(1000);
-        // set PB3 low
-        PORTB = 0b00000000;
-        _delay_ms(5000);
 
-        // flash# 2:
-        // set PB3 high
-        /* PORTB = 0b00001000; 
-        _delay_ms(200);
-        // set PB3 low
-        PORTB = 0b00000000;
-        _delay_ms(200); */
+
+
     }
  
   return 1;
+}
+
+void blinkPb1()
+{
+    // set PB1 high
+    PORTB = 0b00000010;
+    _delay_ms(5000);
+    // set PB1 low
+    PORTB = 0b00000000;
+    _delay_ms(200);
+
+    for (i = 1; i < 3; i++) {
+        PORTB = 0b00000010;
+        _delay_ms(200);
+        // set PB1 low
+        PORTB = 0b00000000;
+        _delay_ms(200);
+    }
+}
+
+void blinkPb3()
+{
+    // set PB3 high
+    PORTB = 0b00001000; 
+    _delay_ms(5000);
+    // set PB3 low
+    PORTB = 0b00000000;
+    _delay_ms(200);
+
+    for (i = 1; i < 3; i++) {
+        PORTB = 0b00001000;
+        _delay_ms(200);
+        // set PB1 low
+        PORTB = 0b00000000;
+        _delay_ms(200);
+    }
+}
+
+void blinkPb4()
+{
+    // set PB4 high
+    PORTB = 0b00010000;
+    _delay_ms(5000);
+    // set PB4 low
+    PORTB = 0b00000000;
+    _delay_ms(200);
+
+    for (i = 1; i < 3; i++) {
+        PORTB = 0b00010000;
+        _delay_ms(200);
+        // set PB1 low
+        PORTB = 0b00000000;
+        _delay_ms(200);
+    }
 }
